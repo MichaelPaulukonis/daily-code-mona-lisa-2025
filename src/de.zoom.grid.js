a85efe4f481c194b4100747971dee71e54071541
Michael Paulukonis <michael.paulukonis@gmail.com>
2025-01-05 12:15:07 -0500

    Links to editor.p5 scripts, basic linting

--- src/de.zoom.grid.js
+++ src/de.zoom.grid.js
@@ -1,16 +1,17 @@
-let step = 0; 
-let gridIndex = 0;
-let shrinkSteps;
-const gridCount = 5; // assuming a square, so really gridCount x gridCount
-const targetSize = 1080; 
-const tileSize = Math.floor(targetSize / gridCount);
-const initialShrinkSteps = 20;
-const minShrinkSteps = 5;
-let img;
-let pg;
-let gridPositions = [];
-let autoSave = true;
+// https://editor.p5js.org/MichaelPaulukonis/sketches/tgLKUyTMK
 
+let step = 0
+let gridIndex = 0
+let shrinkSteps
+const gridCount = 5 // assuming a square, so really gridCount x gridCount
+const targetSize = 1080
+const tileSize = Math.floor(targetSize / gridCount)
+const initialShrinkSteps = 20
+const minShrinkSteps = 5
+let img
+let pg
+let gridPositions = []
+const autoSave = true
 
 const timestamp = () => {
   const d = new Date()
@@ -24,89 +25,89 @@ const timestamp = () => {
   return `${year}${month}${day}.${hour}${min}${secs}.${millis}`
 }
 
-  function generateFilename () {
-    return `little_suggestion_grid.${timestamp()}.png`
-  }
-
-function easeInQuad(t) {
-  return t * t;
+function generateFilename () {
+  return `little_suggestion_grid.${timestamp()}.png`
 }
 
-function easeInOutQuad(t) {
-  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+function easeInQuad (t) {
+  return t * t
 }
 
-function shrinkImage(p, gridX, gridY) {
-  let t = step / shrinkSteps;
-  let easedT = easeInQuad(t);
-  let size = p.lerp(targetSize, tileSize, easedT);
-  let x = p.lerp(targetSize / 2, gridX * tileSize + tileSize / 2, easedT);
-  let y = p.lerp(targetSize / 2, gridY * tileSize + tileSize / 2, easedT);
+function easeInOutQuad (t) {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+}
+
+function shrinkImage (p, gridX, gridY) {
+  const t = step / shrinkSteps
+  const easedT = easeInQuad(t)
+  const size = p.lerp(targetSize, tileSize, easedT)
+  const x = p.lerp(targetSize / 2, gridX * tileSize + tileSize / 2, easedT)
+  const y = p.lerp(targetSize / 2, gridY * tileSize + tileSize / 2, easedT)
 
   // Set transparency
-  let alpha = p.lerp(100, 255, t);
-  p.tint(255, alpha);
+  const alpha = p.lerp(100, 255, t)
+  p.tint(255, alpha)
 
-  p.image(img, x - size / 2, y - size / 2, size, size);
+  p.image(img, x - size / 2, y - size / 2, size, size)
 
   // Reset tint to default
-  p.noTint();
+  p.noTint()
 }
 
-let sketch = function (p) {
+const sketch = function (p) {
   p.preload = function () {
-    img = p.loadImage("images/mona.png");
-  };
+    img = p.loadImage('images/mona.png')
+  }
 
   p.setup = function () {
-    p.createCanvas(targetSize, targetSize);
-    pg = p.createGraphics(targetSize, targetSize);
+    p.createCanvas(targetSize, targetSize)
+    pg = p.createGraphics(targetSize, targetSize)
     p.frameRate(4)
-    
+
     // Initialize grid positions
     for (let y = 0; y < gridCount; y++) {
       for (let x = 0; x < gridCount; x++) {
-        gridPositions.push({ x, y });
+        gridPositions.push({ x, y })
       }
     }
 
-    gridPositions = p.shuffle(gridPositions);
-    shrinkSteps = getShrinkSteps(gridIndex, gridPositions.length);
-    
-    p.background(255);
-    if (autoSave) p.saveCanvas(generateFilename());
-  };
+    gridPositions = p.shuffle(gridPositions)
+    shrinkSteps = getShrinkSteps(gridIndex, gridPositions.length)
 
-  function getShrinkSteps(gridIndex, totalGrids) {
-    let t = gridIndex / totalGrids;
-    let easedT = easeInOutQuad(t);
-    return Math.floor(p.lerp(initialShrinkSteps, minShrinkSteps, easedT));
+    p.background(255)
+    if (autoSave) p.saveCanvas(generateFilename())
+  }
+
+  function getShrinkSteps (gridIndex, totalGrids) {
+    const t = gridIndex / totalGrids
+    const easedT = easeInOutQuad(t)
+    return Math.floor(p.lerp(initialShrinkSteps, minShrinkSteps, easedT))
   }
 
   p.draw = function () {
-    p.background(255);
-    p.image(pg, 0, 0); // Draw the completed grid squares
+    p.background(255)
+    p.image(pg, 0, 0) // Draw the completed grid squares
 
     if (step < shrinkSteps) {
-      let { x: gridX, y: gridY } = gridPositions[gridIndex];
-      shrinkImage(p, gridX, gridY);
-      step++;
+      const { x: gridX, y: gridY } = gridPositions[gridIndex]
+      shrinkImage(p, gridX, gridY)
+      step++
     } else {
-      let { x: gridX, y: gridY } = gridPositions[gridIndex];
-      pg.image(img, gridX * tileSize, gridY * tileSize, tileSize, tileSize); // Draw the final position in the current grid square to the PGraphics object
-      p.image(pg, 0, 0);
-      shrinkSteps = getShrinkSteps(gridIndex, gridPositions.length);
-      step = 0; // Reset step counter
-      gridIndex++; // Move to the next grid position
+      const { x: gridX, y: gridY } = gridPositions[gridIndex]
+      pg.image(img, gridX * tileSize, gridY * tileSize, tileSize, tileSize) // Draw the final position in the current grid square to the PGraphics object
+      p.image(pg, 0, 0)
+      shrinkSteps = getShrinkSteps(gridIndex, gridPositions.length)
+      step = 0 // Reset step counter
+      gridIndex++ // Move to the next grid position
       if (gridIndex >= gridPositions.length) {
-        p.image(pg, 0, 0);
-        p.noLoop();
-        gridIndex = 0; // Reset index if all positions are filled
+        p.image(pg, 0, 0)
+        p.noLoop()
+        gridIndex = 0 // Reset index if all positions are filled
       }
     }
-    
-     if (autoSave) p.saveCanvas(generateFilename());
-  };
-};
 
-new p5(sketch);
+    if (autoSave) p.saveCanvas(generateFilename())
+  }
+}
+
+new p5(sketch)
--- src/supershifty.rose.thief.js
+++ src/supershifty.rose.thief.js
@@ -1,30 +1,30 @@
-// https://editor.p5js.org/MichaelPaulukonis/sketches/99iMKW8DC
+// // https://editor.p5js.org/MichaelPaulukonis/sketches/mLtUitmYT
 
-function sketch(p) {
-  let img;
-  let xOffsets = [0, 0, 0];
-  let yOffsets = [0, 0, 0];
-  let easing = 0.3;
-  let dragging = false;
-  let dragStart = null; // vector
-  let needsRedraw = true;
-  let autoSave = false;
+function sketch (p) {
+  let img
+  let xOffsets = [0, 0, 0]
+  let yOffsets = [0, 0, 0]
+  const easing = 0.3
+  let dragging = false
+  let dragStart = null // vector
+  let needsRedraw = true
+  let autoSave = false
 
   p.preload = function () {
-    img = p.loadImage("images/mona.png");
-  };
+    img = p.loadImage('images/mona.png')
+  }
 
   p.setup = function () {
-    img.resize(600, 0);
-    p.createCanvas(img.width, img.height);
-  };
+    img.resize(600, 0)
+    p.createCanvas(img.width, img.height)
+  }
 
   p.draw = function () {
     if (needsRedraw) {
-      const processedImg = shiftImage(img, xOffsets, yOffsets);
-      p.image(processedImg, 0, 0);
-      if (autoSave) p.saveCanvas(`supershiftrosethief.${p.frameCount}.png`);
-      needsRedraw = false;
+      const processedImg = shiftImage(img, xOffsets, yOffsets)
+      p.image(processedImg, 0, 0)
+      if (autoSave) p.saveCanvas(`supershiftrosethief.${p.frameCount}.png`)
+      needsRedraw = false
     }
 
     if (!dragging) {
@@ -32,61 +32,61 @@ function sketch(p) {
         // Ease back to 0
         for (let i = 0; i < 3; i++) {
           xOffsets[i] -=
-            Math.sign(xOffsets[i]) * easing * Math.abs(xOffsets[i]);
+            Math.sign(xOffsets[i]) * easing * Math.abs(xOffsets[i])
           yOffsets[i] -=
-            Math.sign(yOffsets[i]) * easing * Math.abs(yOffsets[i]);
+            Math.sign(yOffsets[i]) * easing * Math.abs(yOffsets[i])
         }
-        needsRedraw = true;
+        needsRedraw = true
       } else {
-        autoSave = false;
+        autoSave = false
       }
     }
-  };
+  }
 
   p.mousePressed = function () {
-    dragStart = new p5.Vector(p.mouseX, p.mouseY);
-  };
+    dragStart = new p5.Vector(p.mouseX, p.mouseY)
+  }
 
   p.mouseDragged = function () {
-    dragging = true;
-    autoSave = true;
-    let dx = p.mouseX - dragStart.x;
-    let dy = p.mouseY - dragStart.y;
-    xOffsets = [dx * 0.8, dx * 1.2, dx * 1.8];
-    yOffsets = [dy * 0.8, dy * 1.2, dy * 1.8];
-    needsRedraw = true;
-  };
+    dragging = true
+    autoSave = true
+    const dx = p.mouseX - dragStart.x
+    const dy = p.mouseY - dragStart.y
+    xOffsets = [dx * 0.8, dx * 1.2, dx * 1.8]
+    yOffsets = [dy * 0.8, dy * 1.2, dy * 1.8]
+    needsRedraw = true
+  }
 
   p.mouseReleased = function () {
-    dragging = false;
-  };
+    dragging = false
+  }
 
   const shiftImage = (img, xOffsets = [0, 0, 0], yOffsets = [0, 0, 0]) => {
-    let processedImg = p.createImage(img.width, img.height);
-    img.loadPixels();
-    processedImg.loadPixels();
+    const processedImg = p.createImage(img.width, img.height)
+    img.loadPixels()
+    processedImg.loadPixels()
 
     for (let y = 0; y < img.height; y++) {
       for (let x = 0; x < img.width; x++) {
         for (let c = 0; c < 3; c++) {
-          let xOffset = Math.round(xOffsets[c]);
-          let yOffset = Math.round(yOffsets[c]);
-          let newX = (x + xOffset + img.width) % img.width;
-          let newY = (y + yOffset + img.height) % img.height;
-          let newIndex = (newX + newY * img.width) * 4;
-          let index = (x + y * img.width) * 4;
-          processedImg.pixels[newIndex + c] = img.pixels[index + c];
+          const xOffset = Math.round(xOffsets[c])
+          const yOffset = Math.round(yOffsets[c])
+          const newX = (x + xOffset + img.width) % img.width
+          const newY = (y + yOffset + img.height) % img.height
+          const newIndex = (newX + newY * img.width) * 4
+          const index = (x + y * img.width) * 4
+          processedImg.pixels[newIndex + c] = img.pixels[index + c]
         }
         processedImg.pixels[(x + y * img.width) * 4 + 3] =
-          img.pixels[(x + y * img.width) * 4 + 3]; // Copy alpha channel
+          img.pixels[(x + y * img.width) * 4 + 3] // Copy alpha channel
       }
     }
 
-    processedImg.updatePixels();
-    return processedImg;
-  };
+    processedImg.updatePixels()
+    return processedImg
+  }
 
-  const isEasingDone = (arr) => arr.every((val) => Math.abs(val) <= 1);
+  const isEasingDone = (arr) => arr.every((val) => Math.abs(val) <= 1)
 }
 
-new p5(sketch);
+new p5(sketch)

0086705cf94c54719212662c98f073ff3b5760d7
Michael Paulukonis <michael.paulukonis@gmail.com>
2025-02-02 09:57:08 -0500

    Updated 02.01 to switch out image on move Added more image-masks

--- src/2025.02.01.masking.redux/sketch.js
+++ src/2025.02.01.masking.redux/sketch.js
@@ -35,21 +35,31 @@ const sketch = function (p) {
   let pause = false
   const positions = []
   let staticPositions = []
+  let usedMasks = []
+  let autoSave = false
+  let autoSaveCount = 0
+  const totalFrames = 1000
+  let boxHeight = 0
 
   p.preload = function () {
     colorImg = p.loadImage('images/mona_square.jpeg')
     masks = [
-      p.loadImage('images/splat.00.png'),
-      p.loadImage('images/splat.01.png'),
-      p.loadImage('images/thumbprint.01.png'),
-      p.loadImage('images/tow.zone.png'),
-      p.loadImage('images/vcs.01.png'),
-      p.loadImage('images/watch.more.tv.png'),
+      p.loadImage('images/5mg.logo.01.png'),
+      p.loadImage('images/cows.duo.01.png'),
+      p.loadImage('images/damien.hirst.01.png'),
+      p.loadImage('images/fire.door.png'),
       p.loadImage('images/gottlieb.01.png'),
       p.loadImage('images/green.beans.01.png'),
+      p.loadImage('images/john.deere.00.png'),
       p.loadImage('images/napster.png'),
       p.loadImage('images/pbs.png'),
-      p.loadImage('images/peanuts.lucy.00.png')
+      p.loadImage('images/peanuts.lucy.00.png'),
+      p.loadImage('images/splat.00.png'),
+      p.loadImage('images/splat.01.png'),
+      p.loadImage('images/splat.03.png'),
+      p.loadImage('images/thumbprint.01.png'),
+      p.loadImage('images/tow.zone.png'),
+      p.loadImage('images/vcs.01.png')
     ]
   }
 
@@ -58,7 +68,13 @@ const sketch = function (p) {
     const shuffledPositions = p.shuffle(positions)
 
     // Only use as many positions as we have masks
-    staticPositions = shuffledPositions.slice(0, masks.length)
+    staticPositions = shuffledPositions.slice(0, 10).map((pos, index) => ({
+      position: pos,
+      mask: masks[index]
+    }))
+
+    // Track used masks
+    usedMasks = masks.slice(0, 10)
   }
 
   p.setup = function () {
@@ -66,7 +82,7 @@ const sketch = function (p) {
     p.createCanvas(600, 600).drop(handleFile)
     p.frameRate(15)
 
-    gridSize = 4
+    gridSize = 5
     inset = 20
     cellWidth = targetLayer.width / gridSize
     cellHeight = targetLayer.height / gridSize
@@ -87,13 +103,14 @@ const sketch = function (p) {
         positions.push({ row, col })
       }
     }
-  }
 
-  p.draw = function () {
-    if (!pause) {
-      updateTargetLayer()
-    }
-    p.image(targetLayer, 0, 0, p.width, p.height)
+    const displayPanel = p.createDiv('')
+    displayPanel.id('displayPanel')
+    displayPanel.style('background-color', 'rgba(0, 0, 0, 0.6)')
+    displayPanel.style('border-radius', '10px')
+    displayPanel.style('padding', '10px')
+    displayPanel.style('color', 'white')
+    displayPanel.style('font-size', '16px')
   }
 
   function updateTargetLayer () {
@@ -104,19 +121,28 @@ const sketch = function (p) {
     } else {
       // Find a new random unpopulated position
       const unpopulatedPositions = positions.filter(
-        pos => !staticPositions.includes(pos)
+        pos => !staticPositions.some(sp => sp.position === pos)
       )
       const newPosition = p.random(unpopulatedPositions)
 
-      // Move the first cell to the new position and then to the end of the list
-      staticPositions.push(newPosition)
-      staticPositions.shift()
+      // Find a new random unused mask
+      const unusedMasks = masks.filter(mask => !usedMasks.includes(mask))
+      const newMask = p.random(unusedMasks)
+
+      // Move the first cell to the new position and assign a new mask
+      const firstCell = staticPositions.shift()
+      firstCell.position = newPosition
+      firstCell.mask = newMask
+      staticPositions.push(firstCell)
+
+      // Update used masks
+      usedMasks = staticPositions.map(sp => sp.mask)
     }
 
     // Draw using staticPositions
-    staticPositions.forEach((pos, index) => {
-      const cellX = pos.col * cellWidth
-      const cellY = pos.row * cellHeight
+    staticPositions.forEach(({ position, mask }) => {
+      const cellX = position.col * cellWidth
+      const cellY = position.row * cellHeight
 
       const maskX = cellX + inset + (insetWidth - newWidth) / 2
       const maskY = cellY + inset + (insetHeight - newHeight) / 2
@@ -132,8 +158,8 @@ const sketch = function (p) {
       )
       targetLayer.drawingContext.clip()
 
-      // Use each mask exactly once
-      targetLayer.image(masks[index], maskX, maskY, newWidth, newHeight)
+      // Use the associated mask
+      targetLayer.image(mask, maskX, maskY, newWidth, newHeight)
 
       targetLayer.drawingContext.globalCompositeOperation = 'lighten'
       targetLayer.image(
@@ -149,9 +175,39 @@ const sketch = function (p) {
     })
   }
 
+  p.draw = function () {
+    if (!pause) {
+      updateTargetLayer()
+      p.image(targetLayer, 0, 0, p.width, p.height)
+      if (autoSave) {
+        if (autoSaveCount >= totalFrames) {
+          autoSave = false
+          p.frameRate(60)
+          return
+        }
+
+        targetLayer.save(
+          generateFilename(
+            `mona-masks_${String(autoSaveCount).padStart(4, '0')}`
+          )
+        )
+        autoSaveCount++
+      }
+    }
+    showDisplayPanel()
+  }
+
   p.keyPressed = () => {
-    if (p.key === 'S') {
-      p.save(generateFilename('mona-mask'))
+    if (p.key === 'A') {
+      autoSave = !autoSave
+      if (autoSave) {
+        p.frameRate(5)
+        autoSaveCount = 0
+      } else {
+        p.frameRate(60)
+      }
+    } else if (p.key === 'S') {
+      p.save(generateFilename('mona-masks'))
     } else if (p.key === 'r') {
       initializePositions()
     } else if (p.key === ' ') {
@@ -173,6 +229,30 @@ const sketch = function (p) {
     } else {
       console.log('Not an image file!')
     }
+  }
+
+  const showDisplayPanel = () => {
+    const uiText = [
+      `Autosave: ${autoSave}`,
+      autoSave ? `Frame: ${autoSaveCount} / ${totalFrames}` : '',
+      `masks: ${masks.length}`,
+      `canvas: ${p.width}x${p.height}`
+    ].filter(Boolean) // removes empty strings
+
+    const displayPanel = document.getElementById('displayPanel')
+    displayPanel.innerHTML = '' // Clear previous content
+
+    uiText.forEach(text => {
+      const textElement = document.createElement('div')
+      textElement.textContent = text
+      displayPanel.appendChild(textElement)
+    })
+
+    // Calculate boxHeight based on the content
+    boxHeight = displayPanel.clientHeight
+    displayPanel.style.position = 'absolute'
+    displayPanel.style.left = '5px'
+    displayPanel.style.top = `${p.height - boxHeight - 5}px`
   }
 }
 
